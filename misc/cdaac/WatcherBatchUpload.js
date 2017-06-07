5ee66876befe203ba48c2c62756843d0c14f6190
Yu-Cheng Lin <infwonder@gmail.com>
2017-06-07 12:51:32 -0600

    changing how batch upload threshold works

--- misc/cdaac/WatcherBatchUpload.js
+++ misc/cdaac/WatcherBatchUpload.js
@@ -18,23 +18,23 @@ var inpaths = inpath.split(',');
 ec.peers = JSON.parse(fs.readFileSync("./peers.json"));
 ec.outdir = '/tmp';
 ec.indir = process.argv[2];
-ep.triggerTime = 100; // 0.1 secs
-ep.maxsize = 1048576; // 1 MB
+ep.triggerTime = 2100; // 2.1 secs
+ep.maxsize = 671088640; // 640 MB
 
 connect().then( () => 
   { 
     ec.init_watcher_upload(() => {
       ep.watcher(ec.indir, (error, totalsize, filelist) => 
       {
-        console.log("New batch of files arrived: totalsize = " + totalsize + ", list: " + JSON.stringify(filelist, null, 2));
-        setTimeout(() => { ec.new_qTask_upload(filelist, (err) => { if (err) throw(err); }); }, 2900);
+        console.log("New batch of files arrived: \ntotalsize = " + totalsize + ", \nlist: \n" + JSON.stringify(filelist, null, 2));
+        ec.new_qTask_upload(filelist, (err) => { if (err) throw(err); });
       });
     });
 
     ec.ULJobQ.on('success', (result, job) => 
     {
-      console.log(JSON.stringify(result, null, 2));
-      console.log(JSON.stringify(job, null, 2));
+      console.log("\n ******** File Batch Uploaded ******** \n" + JSON.stringify(result, null, 2));
+      console.log("\n Batch Depth: " + ec.ULJobQ.length);
     });
 
     ec.ULJobQ.on('error', (err, job) => { console.log("Queue job error: " + err); ec.client.shutdown(); });
